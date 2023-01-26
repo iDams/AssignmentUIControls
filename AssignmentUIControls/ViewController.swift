d7c721965a9d4c2dc193b5b0f853aa53872c0fcc
Marco <marcoastorga.g@gmail.com>
2023-01-26 11:50:54 -0500

    Initial Commit

---
+++ AssignmentUIControls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssignmentUIControls
+//
+//  Created by Marco Astorga González on 2023-01-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
